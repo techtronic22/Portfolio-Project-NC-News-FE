7cfe6fe8df92f7919afc7ca4a586aecc68c29840
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-10-27 14:51:46 +0100

    11-delete-comment

--- src/Components/ArticleComments.jsx
+++ src/Components/ArticleComments.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { getCommentsByArticleId } from "./api";
-import CommentForm from "./CommentForm";  
+import CommentForm from "./CommentForm";
 
-const ArticleComments = ({ article_id, username }) => {  
+const ArticleComments = ({ article_id, username }) => {
 	const [comments, setComments] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
@@ -14,13 +14,12 @@ const ArticleComments = ({ article_id, username }) => {
 				setComments(response.data.comments);
 				setLoading(false);
 			})
-            .catch((error) => {
+			.catch((error) => {
 				console.error("Error fetching articles:", error);
 				setLoading(false);
 				setError("An error occurred while fetching comments.");
 			});
-          }, [article_id]);
-			
+	}, [article_id]);
 
 	if (loading) {
 		return <p>Loading comments...</p>;
@@ -30,32 +29,31 @@ const ArticleComments = ({ article_id, username }) => {
 		return <p>Error: {error}</p>;
 	}
 
-
 	return (
-    <section>
-        <h2>Comments</h2>
+		<section>
+			<h2>Comments</h2>
 
-        <CommentForm
-            article_id={article_id}
-            username={username}
-            comments={comments}
-            setComments={setComments}
-        />
+			<CommentForm
+				article_id={article_id}
+				username={username}
+				comments={comments}
+				setComments={setComments}
+			/>
 
-        {comments.length === 0 ? (
-            <p>No comments available for this article.</p>
-        ) : (
-            <ul> 
-                {comments.map((comment) => (
-                    <li key={comment.comment_id}>
-                        <p>{comment.body}</p>
-                        <p>By: {comment.author}</p>
-                    </li>
-                ))}
-            </ul>
-        )}
-    </section>
+			{comments.length === 0 ? (
+				<p>No comments available for this article.</p>
+			) : (
+				<ul>
+					{comments.map((comment) => (
+						<li key={comment.comment_id}>
+							<p>{comment.body}</p>
+							<p>By: {comment.author}</p>
+						</li>
+					))}
+				</ul>
+			)}
+		</section>
 	);
-}
+};
 
 export default ArticleComments;
--- src/Components/CommentForm.jsx
+++ src/Components/CommentForm.jsx
@@ -1,67 +1,112 @@
 import React, { useState } from "react";
-import { postComment, getCommentsByArticleId } from "./api";
+import { postComment, deleteCommentById, getCommentsByArticleId } from "./api";
 
 const CommentForm = ({ article_id, username, comments, setComments }) => {
-  const [newComment, setNewComment] = useState("");
-  const [isFormValid, setIsFormValid] = useState(true);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+	const [newComment, setNewComment] = useState("");
+	const [isFormValid, setIsFormValid] = useState(true);
+	const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
+	const handleCommentChange = (e) => {
+		setNewComment(e.target.value);
 
+		if (e.target.value.trim() !== "") {
+			setIsFormValid(true);
+		}
+	};
 
-    if (e.target.value.trim() !== "") {
-      setIsFormValid(true);
-    }
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+		setIsSubmitting(true);
 
-    setIsSubmitting(true); 
+		if (newComment.trim() === "") {
+			setIsFormValid(false);
+			setIsSubmitting(false);
+			return;
+		}
 
-    if (newComment.trim() === "") {
-      setIsFormValid(false);
-      setIsSubmitting(false); 
-      return;
-    }
+		const optimisticComment = {
+			author: username,
+			body: newComment,
+			comment_id: Date.now(),
+		};
 
-    const optimisticComment = {
-      author: username,
-      body: newComment,
-    };
+		setComments([optimisticComment, ...comments]);
 
-    setComments([optimisticComment, ...comments]);
+		postComment(article_id, optimisticComment)
+			.then((postedComment) => {
+				setComments((prevComments) =>
+					prevComments.map((c) =>
+						c.comment_id === optimisticComment.comment_id ? postedComment : c
+					)
+				);
+				setIsSubmitting(false);
+			})
+			.catch((error) => {
+				setComments((prevComments) =>
+					prevComments.filter(
+						(c) => c.comment_id !== optimisticComment.comment_id
+					)
+				);
+				setIsSubmitting(false);
+			});
 
-    postComment(article_id, optimisticComment)
-      .then((postedComment) => {
-        setComments(prevComments => [postedComment, ...prevComments]);
-        setIsSubmitting(false);
-      })
-      .catch((error) => {
-        console.error("Error posting comment:", error);
-        setComments(prevComments => prevComments.filter((c) => c !== optimisticComment));
-        setIsSubmitting(false);
-      });
+		setNewComment("");
+	};
 
-    setNewComment("");
-  };
+	const handleDelete = (comment_id) => {
+		setComments((prevComments) =>
+			prevComments.filter((comment) => comment.comment_id !== comment_id)
+		);
 
-  return (
-    <div>
-      <textarea
-        value={newComment}
-        onChange={handleCommentChange}
-        placeholder="Add a comment..."
-      />
-      {!isFormValid && (
-        <p style={{ color: "red" }}>Please fill out the comment field.</p>
-      )}
-      <button onClick={handleSubmit} disabled={!isFormValid || isSubmitting}>
-        {isSubmitting ? "Submitting..." : "Submit Comment"}
-      </button>
-    </div>
-  );
+		setIsSubmitting(true);
+
+		deleteCommentById(comment_id)
+			.then(() => {
+				setIsSubmitting(false);
+			})
+			.catch((error) => {
+				setComments((prevComments) =>
+					prevComments.filter((c) => c.comment_id !== comment_id)
+				);
+				setIsSubmitting(false);
+			});
+	};
+
+	return (
+		<div>
+			<textarea
+				value={newComment}
+				onChange={handleCommentChange}
+				placeholder="Add a comment..."
+			/>
+			{!isFormValid && (
+				<p style={{ color: "red" }}>Please fill out the comment field.</p>
+			)}
+			<button onClick={handleSubmit} disabled={!isFormValid || isSubmitting}>
+				{isSubmitting ? "Submitting..." : "Submit Comment"}
+			</button>
+
+			{comments.length > 0 && (
+				<ul>
+					{comments.map((comment) => (
+						<li key={comment.comment_id}>
+							<p>{comment.body}</p>
+							<p>By: {comment.author}</p>
+							{comment.author === username && (
+								<button
+									key={`delete-${comment.comment_id}`}
+									onClick={() => handleDelete(comment.comment_id)}
+									disabled={isSubmitting}>
+									{isSubmitting ? "Deleting..." : "Delete Comment"}
+								</button>
+							)}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
 
 export default CommentForm;
